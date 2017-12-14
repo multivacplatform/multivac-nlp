fa185d3f808bdd9c4e9fd999823f611fdeadff65
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2017-12-14 12:48:16 +0100

    adding tests for spark-nlp

---
+++ src/main/scala/Spark_NLP_Test.scala
@@ -0,0 +1,80 @@
+import org.apache.spark.ml.{Pipeline, PipelineModel, PipelineStage}
+import com.johnsnowlabs.nlp._
+import com.johnsnowlabs.nlp.annotators._
+import com.johnsnowlabs.nlp.annotators.sbd.pragmatic.SentenceDetectorModel
+import com.johnsnowlabs.nlp.annotators.pos.perceptron.PerceptronApproach
+import org.apache.spark.ml.feature.StopWordsRemover
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+
+object Spark_NLP_Test {
+  def run(spark: SparkSession): Unit ={
+
+    println("Spark Version", spark.version)
+
+    val inputFile="src/main/resources/enwikinews.json"
+
+    val df = spark.read.format("json").option("mode", "DROPMALFORMED").load(inputFile)
+
+    val newsDF = df.select("id", "title").filter("id IS NOT NULL AND title IS NOT NULL")
+    newsDF.show(10)
+
+    val documentAssembler = new DocumentAssembler()
+      .setInputCol("title")
+      .setOutputCol("document")
+
+    val sentenceDetector = new SentenceDetectorModel()
+      .setInputCols(Array("document"))
+      .setOutputCol("sentence")
+
+    val regexTokenizer = new RegexTokenizer()
+      .setInputCols(Array("sentence"))
+      .setOutputCol("token")
+
+    val stopwords = spark.read.textFile("src/main/resources/stopwords_en.txt").collect()
+    val filteredTokens = new StopWordsRemover()
+      .setStopWords(stopwords)
+      .setCaseSensitive(false)
+      .setInputCol("token")
+      .setOutputCol("filtered")
+
+    val normalizer = new Normalizer()
+      .setInputCols(Array("token"))
+      .setOutputCol("normalized")
+
+    val stemmer = new Stemmer()
+      .setInputCols(Array("token"))
+      .setOutputCol("stem")
+
+    val posTagger = new PerceptronApproach()
+      .setCorpusPath("src/main/resources/anc-pos-corpus")
+      .setNIterations(20)
+      .setInputCols(Array("sentence", "token"))
+      .setOutputCol("pos")
+
+    val finisher = new Finisher()
+      .setInputCols("token")
+      .setCleanAnnotations(false)
+
+    val pipeline = new Pipeline()
+      .setStages(Array(
+        documentAssembler,
+        sentenceDetector,
+        regexTokenizer,
+        normalizer,
+        stemmer,
+        posTagger,
+        finisher
+      ))
+
+    val pipeLineDF = pipeline
+      .fit(newsDF)
+      .transform(newsDF)
+
+    pipeLineDF.printSchema()
+
+    pipeLineDF.select("token")show(20, false)
+
+    spark.close()
+  }
+}
