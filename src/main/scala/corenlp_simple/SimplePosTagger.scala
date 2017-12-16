5ca63340b339e21730abffb033a8fba079ca5d8a
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2017-12-16 14:36:31 +0100

    add new transformer for CoreNLP Pos Tagger

---
+++ src/main/scala/corenlp_simple/SimplePosTagger.scala
@@ -0,0 +1,24 @@
+package corenlp_simple
+
+import org.apache.spark.ml.UnaryTransformer
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.sql.types.{ArrayType, DataType, StringType}
+
+class SimplePosTagger(override val uid: String)
+  extends UnaryTransformer[String, Seq[String], SimplePosTagger]  {
+
+  def this() = this(Identifiable.randomUID("SimplePosTagger"))
+
+  override protected def createTransformFunc: String => Seq[String] = {
+    SimpleAPI_Functions.getPOS _
+
+  }
+
+  override protected def validateInputType(inputType: DataType): Unit = {
+    require(inputType == StringType)
+  }
+
+  override protected def outputDataType: DataType = {
+    new ArrayType(StringType, true)
+  }
+}
