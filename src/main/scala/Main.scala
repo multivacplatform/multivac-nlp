7322775502daf38a9d082db1c6f86cbe195c57e7
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2017-12-27 21:50:39 +0100

    add file path parameter to Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,9 +5,11 @@ object Main {
   def main(args: Array[String]) {
     val env =args(0)
     val spark = SessionBuilder.buildSession(env)
+    val inputFile="src/main/resources/enwikinews.json"
 
     //    CoreNLP_SimpleAPI_Test.test()
     //    Spark_NLP_Test.test(spark)
-    Test_NLP_Libraries.Test_EnWiki(spark)
+
+    Test_NLP_Libraries.Test_EnWiki(spark, inputFile)
   }
 }
