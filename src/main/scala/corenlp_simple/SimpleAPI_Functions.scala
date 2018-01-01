5831e6adc668f311bf9bf9203c858d8f88de9b28
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-01 23:16:52 +0100

    add french pos function

--- src/main/scala/corenlp_simple/SimpleAPI_Functions.scala
+++ src/main/scala/corenlp_simple/SimpleAPI_Functions.scala
@@ -4,17 +4,25 @@ import scala.collection.JavaConverters._
 import java.util.Properties
 import edu.stanford.nlp.simple._
 
-
 object SimpleAPI_Functions extends Serializable {
+
   def getWords(document: String): Seq[String] = {
     new Sentence(document)
       .words()
       .asScala
   }
 
-  def getPOS(document: String, lang: String): Seq[String] = {
+  def getPOS(document: String): Seq[String] = {
+    new Sentence(document)
+      .posTags()
+      .asScala
+  }
+
+  def getPOSFrench(document: String): Seq[String] = {
     val props = new Properties()
-    props.setProperty("tokenize.language", lang)
+    props.setProperty("tokenize.language", "fr")
+    props.setProperty("language", "french")
+    props.setProperty("pos.model", "edu/stanford/nlp/models/pos-tagger/french/french.tagger")
 
     new Sentence(document, props)
       .posTags(props)
