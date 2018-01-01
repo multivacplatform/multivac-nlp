87dd748ebc50259eb3c71f69a9e6981fd0947a08
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-01 17:54:23 +0100

    test pos tagger with french

--- src/main/scala/corenlp_simple/SimpleAPI_Functions.scala
+++ src/main/scala/corenlp_simple/SimpleAPI_Functions.scala
@@ -1,7 +1,9 @@
 package corenlp_simple
 
 import scala.collection.JavaConverters._
+import java.util.Properties
 import edu.stanford.nlp.simple._
+
 
 object SimpleAPI_Functions extends Serializable {
   def getWords(document: String): Seq[String] = {
@@ -10,9 +12,12 @@ object SimpleAPI_Functions extends Serializable {
       .asScala
   }
 
-  def getPOS(document: String): Seq[String] = {
-    new Sentence(document)
-      .posTags()
+  def getPOS(document: String, lang: String): Seq[String] = {
+    val props = new Properties()
+    props.setProperty("tokenize.language", lang)
+
+    new Sentence(document, props)
+      .posTags(props)
       .asScala
   }
 }
