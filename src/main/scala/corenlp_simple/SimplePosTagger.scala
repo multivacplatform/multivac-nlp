b2806a7369e50c20f259e8646c55aae16ec095ab
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-01 23:13:50 +0100

    split french function

--- src/main/scala/corenlp_simple/SimplePosTagger.scala
+++ src/main/scala/corenlp_simple/SimplePosTagger.scala
@@ -5,14 +5,12 @@ import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.ml.param.Param
 import org.apache.spark.sql.types.{ArrayType, DataType, StringType}
 
-
 class SimplePosTagger(override val uid: String) extends UnaryTransformer[String, Seq[String], SimplePosTagger]  {
-
 
   def this() = this(Identifiable.randomUID("SimplePosTagger"))
 
   override protected def createTransformFunc: String => Seq[String] = {
-    SimpleAPI_Functions.getPOS (_, "fr")
+    SimpleAPI_Functions.getPOS _
   }
 
   override protected def validateInputType(inputType: DataType): Unit = {
