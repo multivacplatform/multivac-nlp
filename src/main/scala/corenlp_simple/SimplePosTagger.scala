390148f3b05f14ec1706bd22427e39a48051320d
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-01 17:54:42 +0100

    test pos tagger with french

--- src/main/scala/corenlp_simple/SimplePosTagger.scala
+++ src/main/scala/corenlp_simple/SimplePosTagger.scala
@@ -2,16 +2,17 @@ package corenlp_simple
 
 import org.apache.spark.ml.UnaryTransformer
 import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.ml.param.Param
 import org.apache.spark.sql.types.{ArrayType, DataType, StringType}
 
-class SimplePosTagger(override val uid: String)
-  extends UnaryTransformer[String, Seq[String], SimplePosTagger]  {
+
+class SimplePosTagger(override val uid: String) extends UnaryTransformer[String, Seq[String], SimplePosTagger]  {
+
 
   def this() = this(Identifiable.randomUID("SimplePosTagger"))
 
   override protected def createTransformFunc: String => Seq[String] = {
-    SimpleAPI_Functions.getPOS _
-
+    SimpleAPI_Functions.getPOS (_, "fr")
   }
 
   override protected def validateInputType(inputType: DataType): Unit = {
