e0ffce63032305ec1c918cdde51c6a329b759012
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2018-01-01 23:48:21 +0100

    add new french function

--- src/main/scala/Test_NLP_Libraries.scala
+++ src/main/scala/Test_NLP_Libraries.scala
@@ -1,5 +1,4 @@
-import corenlp_simple.{SimplePosTagger, SimpleTokenizer}
-
+import corenlp_simple.{SimplePosTagger, SimplePosTaggerFrench, SimpleTokenizer}
 import com.johnsnowlabs.nlp.{DocumentAssembler, Finisher}
 import com.johnsnowlabs.nlp.annotators.{Normalizer, RegexTokenizer, Stemmer}
 import com.johnsnowlabs.nlp.annotators.pos.perceptron.PerceptronApproach
@@ -13,10 +12,194 @@ import org.apache.spark.sql.functions._
 
 object Test_NLP_Libraries {
 
-  def Test_EnWiki(
+  def Test_English(
+                    spark: SparkSession,
+                    inputFile: String
+                  ): Unit ={
+
+    var startTime = System.nanoTime()
+    println(s"==========")
+    println(s"Start loading data")
+
+    import spark.implicits._
+
+    val df = spark.read.format("json").option("mode", "DROPMALFORMED").load(inputFile)
+
+    var elapsed = (System.nanoTime() - startTime) / 1e9
+    println(s"Finished loading data.")
+    println(s"Time (sec)\t$elapsed")
+    println(s"==========")
+
+    val textColumnName = "text"
+
+    val newsDF = df
+      .select(textColumnName)
+      .filter("text IS NOT NULL")
+
+    newsDF.cache()
+
+    println("WikiNews Number of articles: ", newsDF.count())
+
+    //spark-nlp functions
+    val documentAssembler = new DocumentAssembler()
+      .setInputCol(textColumnName)
+      .setOutputCol("document")
+
+    val sentenceDetector = new SentenceDetectorModel()
+      .setInputCols(Array("document"))
+      .setOutputCol("sentence")
+
+    val regexTokenizer = new RegexTokenizer()
+      .setInputCols(Array("sentence"))
+      .setOutputCol("token")
+
+    val normalizer = new Normalizer()
+      .setInputCols(Array("token"))
+      .setOutputCol("normalized")
+
+    val stemmer = new Stemmer()
+      .setInputCols(Array("normalized"))
+      .setOutputCol("stem")
+
+    val posTagger = new PerceptronApproach()
+      .setNIterations(5)
+      .setInputCols(Array("sentence", "token"))
+      .setOutputCol("pos")
+
+    //      .setCorpusPath("src/main/resources/anc-pos-corpus")
+
+    val token_finisher = new Finisher()
+      .setInputCols("normalized")
+      .setOutputCols("tokens_array")
+      .setCleanAnnotations(true)
+      .setOutputAsArray(true)
+
+    //CoreNLP functions
+
+    val corenlp_tokenizer = new SimpleTokenizer()
+      .setInputCol(textColumnName)
+      .setOutputCol("corenlp_tokens")
+
+    val corenlp_pos = new SimplePosTagger()
+      .setInputCol(textColumnName)
+      .setOutputCol("pos")
+
+    //Spark ML
+
+    val stopwords = spark.read.textFile(
+      "src/main/resources/stop-words/stopwords_en.txt",
+      "src/main/resources/stop-words/stopwords_fr.txt").collect()
+
+    val filteredTokens = new StopWordsRemover()
+      .setStopWords(stopwords)
+      .setCaseSensitive(false)
+      .setInputCol("tokens_array")
+      .setOutputCol("filtered")
+
+    val cvModel = new CountVectorizer()
+      .setInputCol("filtered")
+      .setOutputCol("rawFeatures")
+      .setVocabSize(1000)
+      .setMinDF(2)
+      .setMinTF(5)
+
+    val hashingTF = new HashingTF()
+      .setInputCol("filtered")
+      .setOutputCol("rawFeatures")
+      .setNumFeatures(20)
+
+    val idf = new IDF()
+      .setInputCol("rawFeatures")
+      .setOutputCol("features")
+
+    val word2Vec = new Word2Vec()
+      .setInputCol("filtered")
+      .setOutputCol("word2vec")
+      .setVectorSize(100)
+      .setMinCount(10)
+      .setMaxIter(20)
+
+    val pipeline = new Pipeline()
+      .setStages(Array(
+        documentAssembler,
+        sentenceDetector,
+        regexTokenizer,
+        normalizer,
+        stemmer,
+        //        posTagger, // will use CoreNLP
+        corenlp_tokenizer,
+        corenlp_pos,
+        token_finisher,
+        filteredTokens,
+        cvModel,
+        idf,
+        word2Vec
+      ))
+
+    startTime = System.nanoTime()
+    println(s"==========")
+    println(s"Fit the Pipeline")
+
+    val model = pipeline.fit(newsDF)
+
+    println(s"==========")
+    println(s"Transform the Pipeline")
+
+    val pipeLineDF = model.transform(newsDF)
+
+    elapsed = (System.nanoTime() - startTime) / 1e9
+    println(s"Finished training and transforming Pipeline")
+    println(s"Time (sec)\t$elapsed")
+    println(s"==========")
+
+    println("peipeline DataFrame Schema: ")
+    pipeLineDF.printSchema()
+
+    //    pipeLineDF.show(40, truncate = true)
+
+    pipeLineDF.select("text", "corenlp_pos").show(50, truncate = false)
+
+    val tokensDF = pipeLineDF
+      .select(explode($"tokens_array").as("value")) //tokens without stop words
+      .groupBy("value")
+      .count
+
+    println("number of unique tokens:", tokensDF.count())
+    println("total number of tokens:")
+    tokensDF.agg(sum("count")).show()
+    println("display top 100 tokens:")
+    tokensDF.sort($"count".desc).show(20, truncate = false)
+
+    val filtteredTokensDF = pipeLineDF
+      .select(explode($"filtered").as("value")) //tokens after stop words
+      .groupBy("value")
+      .count
+
+    println("number of unique tokens after stop words:", filtteredTokensDF.count())
+    println("total number of tokens after stop words:")
+    filtteredTokensDF.agg(sum("count")).show()
+
+    println("display top 100 filtered tokens:")
+    filtteredTokensDF.sort($"count".desc).show(20, truncate = false)
+
+
+    // Vocabs in CountVectorizerModel
+    val cvModelPipeline = model.stages(9).asInstanceOf[CountVectorizerModel]
+    val vocabArray = cvModelPipeline.vocabulary
+    //    vocabArray.foreach(println)
+
+    // word2VecModel
+    val word2VecModel = model.stages.last.asInstanceOf[Word2VecModel]
+    word2VecModel.findSynonyms("london", 4).show(false)
+    word2VecModel.findSynonyms("france", 4).show(false)
+    word2VecModel.findSynonyms("monday", 4).show(false)
+
+    spark.close()
+  }
+
+  def Test_French(
                    spark: SparkSession,
-                   inputFile: String,
-                   lang: String
+                   inputFile: String
                  ): Unit ={
 
     var startTime = System.nanoTime()
@@ -67,6 +250,7 @@ object Test_NLP_Libraries {
       .setNIterations(5)
       .setInputCols(Array("sentence", "token"))
       .setOutputCol("pos")
+
     //      .setCorpusPath("src/main/resources/anc-pos-corpus")
 
     val token_finisher = new Finisher()
@@ -81,9 +265,9 @@ object Test_NLP_Libraries {
       .setInputCol(textColumnName)
       .setOutputCol("corenlp_tokens")
 
-    val corenlp_pos = new SimplePosTagger()
+    val corenlp_pos_french = new SimplePosTaggerFrench()
       .setInputCol(textColumnName)
-      .setOutputCol("corenlp_pos")
+      .setOutputCol("pos")
 
     //Spark ML
 
@@ -127,9 +311,9 @@ object Test_NLP_Libraries {
         regexTokenizer,
         normalizer,
         stemmer,
+        //        posTagger, // will use CoreNLP
         corenlp_tokenizer,
-        corenlp_pos,
-        posTagger,
+        corenlp_pos_french,
         token_finisher,
         filteredTokens,
         cvModel,
@@ -156,7 +340,7 @@ object Test_NLP_Libraries {
     println("peipeline DataFrame Schema: ")
     pipeLineDF.printSchema()
 
-//    pipeLineDF.show(40, truncate = true)
+    //    pipeLineDF.show(40, truncate = true)
 
     pipeLineDF.select("text", "corenlp_pos").show(50, truncate = false)
 
@@ -185,21 +369,16 @@ object Test_NLP_Libraries {
 
 
     // Vocabs in CountVectorizerModel
-    val cvModelPipeline = model.stages(10).asInstanceOf[CountVectorizerModel]
+    val cvModelPipeline = model.stages(9).asInstanceOf[CountVectorizerModel]
     val vocabArray = cvModelPipeline.vocabulary
     //    vocabArray.foreach(println)
 
     // word2VecModel
     val word2VecModel = model.stages.last.asInstanceOf[Word2VecModel]
-    if(lang == "en"){
-      word2VecModel.findSynonyms("london", 4).show(false)
-      word2VecModel.findSynonyms("france", 4).show(false)
-      word2VecModel.findSynonyms("monday", 4).show(false)
-    } else if(lang == "fr"){
-      word2VecModel.findSynonyms("france", 4).show(false)
-      word2VecModel.findSynonyms("politique", 4).show(false)
-      word2VecModel.findSynonyms("lundi", 4).show(false)
-    }
+
+    word2VecModel.findSynonyms("france", 4).show(false)
+    word2VecModel.findSynonyms("politique", 4).show(false)
+    word2VecModel.findSynonyms("lundi", 4).show(false)
 
     spark.close()
   }
